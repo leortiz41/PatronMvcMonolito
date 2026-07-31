dc942e24447e41776bec835dd36d4c910766e487
leortiz41 <60624119+leortiz41@users.noreply.github.com>
2026-07-30 20:36:01 -0600

    Creando Modelos de Autos y Libro

---
+++ BibliotecaMonolito/Models/Autor.cs
@@ -0,0 +1,9 @@
+public class Autor
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Nacionalidad { get; set; } = string.Empty;
+    public List<> Libros { get; set; } = new();
+}   
+
+
---
+++ BibliotecaMonolito/Models/Libro.cs
@@ -0,0 +1,11 @@
+namespace bibliotecaMonolito.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string AnioPublicacion { get; set; } = string.Empty;
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; } = new();
+    }
+}
